3b1629a65ad4b925408cc32f650a4f4d420bfc3c
André Gomes <andre-gomes@live.fr>
2018-08-28 13:50:50 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Dumy Test</h1>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
